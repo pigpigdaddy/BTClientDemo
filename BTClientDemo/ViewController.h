1423b47872014102a626788b2c3ebbb50ff02f59
linfengchina <linfeng@linfeng-iOS.local>
2014-08-22 16:42:37 +0800

    fix bug
    fix bug

--- BTClientDemo/ViewController.h
+++ BTClientDemo/ViewController.h
@@ -2,7 +2,7 @@
 //  ViewController.h
 //  BTClientDemo
 //
-//  Created by 林峰 on 14-8-21.
+//  Created by pigpigdaddy on 14-8-21.
 //  Copyright (c) 2014年 pigpigdaddy. All rights reserved.
 //
 
--- BTClientDemoTests/BTClientDemoTests.m
+++ BTClientDemoTests/BTClientDemoTests.m
@@ -2,7 +2,7 @@
 //  BTClientDemoTests.m
 //  BTClientDemoTests
 //
-//  Created by 林峰 on 14-8-21.
+//  Created by pigpigdaddy on 14-8-21.
 //  Copyright (c) 2014年 pigpigdaddy. All rights reserved.
 //
 
